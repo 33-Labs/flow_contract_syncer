dbba118ab713c67561021bb6897c69a075ebb042
lanford33 <lanfordcai@outlook.com>
2022-12-03 17:58:44 +0800

    feat: change code index to trgm index

--- priv/repo/migrations/20221129155542_add_contract_code_index.exs
+++ priv/repo/migrations/20221129155542_add_contract_code_index.exs
@@ -4,11 +4,14 @@ defmodule FlowContractSyncer.Repo.Migrations.AddContractCodeIndex do
   def up do
     # For full text search
     execute("CREATE EXTENSION pg_trgm")
-    execute("CREATE INDEX IF NOT EXISTS contracts_code_index ON contracts USING GIN (to_tsvector('english', uuid || ' ' || coalesce(code, ' ')))")
+    # execute("CREATE INDEX IF NOT EXISTS contracts_code_index ON contracts USING GIN (to_tsvector('english', uuid || ' ' || coalesce(code, ' ')))")
+    execute("CREATE INDEX IF NOT EXISTS contracts_code_trgm_index ON contracts USING GIN (code gin_trgm_ops)")
+    execute("CREATE INDEX IF NOT EXISTS contracts_uuid_trgm_index ON contracts USING GIN (uuid gin_trgm_ops)")
   end
 
   def down do
-    execute("DROP INDEX contracts_code_index")
+    execute("DROP INDEX contracts_code_trgm_index")
+    execute("DROP INDEX contracts_uuid_trgm_index")
     execute("DROP EXTENSION pg_trgm")
   end
 end
