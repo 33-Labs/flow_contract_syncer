6bc81d3b2b4169664821f61170e139b7cd542128
lanford33 <lanfordcai@outlook.com>
2022-12-05 20:56:14 +0800

    fix: endpoint

--- config/prod.exs
+++ config/prod.exs
@@ -20,16 +20,9 @@ config :logger, level: :info
 # To get SSL working, you will need to add the `https` key
 # to the previous section and set your `:url` port to 443:
 #
-#     config :flow_contract_syncer, FlowContractSyncerWeb.Endpoint,
-#       ...,
-#       url: [host: "example.com", port: 443],
-#       https: [
-#         ...,
-#         port: 443,
-#         cipher_suite: :strong,
-#         keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-#         certfile: System.get_env("SOME_APP_SSL_CERT_PATH")
-#       ]
+config :flow_contract_syncer, FlowContractSyncerWeb.Endpoint,
+  url: [host: "flow-contract-syncer.gigalixirapp.com", port: 443]
+
 #
 # The `cipher_suite` is set to `:strong` to support only the
 # latest and more secure SSL ciphers. This means old browsers
